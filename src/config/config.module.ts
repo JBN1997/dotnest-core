b756b6ff117558cb438af445900b725189c2b72f
Bruno Bengtson Neves <juliobn13@gmail.com>
2023-02-28 18:29:00 -0300

    feat: add database module to config module

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,18 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule as NestConfigModule } from '@nestjs/config';
 import { EnvConfigModule } from '@config/env/env.module';
+import { DataBaseConfigModule } from '@config/db/db.module';
 import environment from '@config/env/env.provider';
+import db from '@config/db/db.provider';
 
 @Module({
    imports: [
       NestConfigModule.forRoot({
-         load: [environment],
+         load: [environment, db],
          isGlobal: true,
          envFilePath: `.env`,
       }),
       EnvConfigModule,
+      DataBaseConfigModule
    ],
    providers: [],
-   exports: [EnvConfigModule],
+   exports: [EnvConfigModule, DataBaseConfigModule],
 })
 export class ConfigModule {}
