74d03ba6f58369fdc30e6fe361b49a6575e0346f
Bruno Bengtson Neves <juliobn13@gmail.com>
2023-03-11 11:26:38 -0300

    refactor: migrate user service to workspace and add base service

--- src/service/user/user.service.ts
+++ src/service/user/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from '@models/users/entities/user.entity';
+import { User } from '@entity/user/user.entity';
 
 @Injectable()
 export class UserService {
