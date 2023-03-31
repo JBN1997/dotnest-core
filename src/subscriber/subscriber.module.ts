f9631e70726c8c86ec669e097a3243f74f7669a6
Bruno Bengtson Neves <juliobn13@gmail.com>
2023-03-31 20:47:48 -0300

    import websocket client module to subscriber and instance new client case handle connection

--- src/subscriber/subscriber.module.ts
+++ src/subscriber/subscriber.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { WebSocketServerGateway } from '@subscriber/websocket-server/websocket-server.gateway';
+import { WebsocketClientModule } from '@subscriber/websocket-client/websocket-client.module';
 
 @Module({
-   imports: [],
+   imports: [WebsocketClientModule],
    controllers: [],
    providers: [WebSocketServerGateway],
    exports: [],
--- src/subscriber/websocket-server/websocket-server.gateway.ts
+++ src/subscriber/websocket-server/websocket-server.gateway.ts
@@ -1,14 +1,22 @@
 import { WebSocketGateway, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage } from '@nestjs/websockets';
 import { Logger } from '@nestjs/common';
 import { Server, Socket } from 'socket.io';
+import { WebsocketClientService } from '@subscriber/websocket-client/services/websocket-client.service';
+import { CreateWebsocketClientDto } from '@subscriber/websocket-client/dto/create-websocket-client.dto';
 
 @WebSocketGateway(2000, { cors: { origin: '*' } })
 export class WebSocketServerGateway implements OnGatewayConnection, OnGatewayDisconnect {
    private readonly logger = new Logger(WebSocketServerGateway.name);
    @WebSocketServer() public server: Server;
 
-   public handleConnection(client: Socket) {
-      this.logger.log(`Socket client ${client.id} conectado`);
+   constructor(private readonly websocketClientService: WebsocketClientService) { }
+
+   public async handleConnection(client: Socket) {
+      this.logger.log(`[WEBSOCKET-SERVER]: Socket client ${client.id} conectado`);
+
+      const websocketClient = new CreateWebsocketClientDto();
+      websocketClient.clientID = client.id;
+      await this.websocketClientService.createWebsocketClient(websocketClient);
    }
 
    public handleDisconnect(client: Socket) {
