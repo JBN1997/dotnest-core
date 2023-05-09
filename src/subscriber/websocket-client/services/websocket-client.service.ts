b618148bfaaac3daa90e14815b292876f25c7aa7
Bruno Bengtson Neves <juliobn13@gmail.com>
2023-05-08 22:20:52 -0300

    feat: handle disconnect websocket client

--- src/subscriber/websocket-client/services/websocket-client.service.ts
+++ src/subscriber/websocket-client/services/websocket-client.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { DeepPartial } from 'typeorm';
 import { WebsocketClientEntity } from '@subscriber/websocket-client/entities/websocket-client.entity';
 import { WebsocketClientRepository } from '@subscriber/websocket-client/repositories/websocket-client.repository';
 import { CreateWebsocketClientDto } from '@subscriber/websocket-client/dto/create-websocket-client.dto';
+import { DeleteWebsocketClientDto } from '@subscriber/websocket-client/dto/delete-websocket-client.dto';
+import { DeleteResult } from 'typeorm/query-builder/result/DeleteResult';
 
 @Injectable()
 export class WebsocketClientService {
@@ -14,5 +15,18 @@ export class WebsocketClientService {
       if (!dto) throw new Error('Missing dto parameter in createWebsocketClient');
 
       return await this.websocketClientRepository.saveAsync({ clientID: dto.clientID });
+   }
+
+   async getWebsocketClientById(id: string): Promise<WebsocketClientEntity> {
+      if (!id) throw new Error('Missing id parameter in getWebsocketClientById');
+
+      return await this.websocketClientRepository.getAsync(id);
+   }
+
+   async deleteWebsocketClientById(id: string): Promise<boolean> {
+      if (!id) throw new Error('Missing id parameter in deleteWebsocketClientById');
+
+      const result: DeleteResult = await this.websocketClientRepository.deleteByIdAsync(id);
+      return result?.affected > 0;
    }
 }
